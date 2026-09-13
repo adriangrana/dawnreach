5e2a79badfcaf94b104fe9fde2f3ccaa57c1a129
Adrian Torres Graña <adriangrana@gmail.com>
2026-09-13 22:37:11 +0200

    feat(app): use Dawnreach PNG icon

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,6 +1,11 @@
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
+    let mut context = tauri::generate_context!();
+    context.set_default_window_icon(Some(tauri::include_image!(
+        "../public/assets/icon/dawnreach.png"
+    )));
+
     tauri::Builder::default()
-        .run(tauri::generate_context!())
+        .run(context)
         .expect("error while running Dawnreach");
 }
