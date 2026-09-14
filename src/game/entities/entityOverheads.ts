476a4985efb21ac63690b6a47a7bbe6de90c1793
Adrian Torres Graña <adriangrana@gmail.com>
2026-09-14 08:43:41 +0200

    chore(overheads): remove obsolete framed-bar helper

--- src/game/entities/entityOverheads.ts
+++ src/game/entities/entityOverheads.ts
@@ -44,24 +44,6 @@ function healthPalette(team: TeamId) {
   }
 }
 
-function roundRect(
-  ctx: CanvasRenderingContext2D,
-  x: number,
-  y: number,
-  width: number,
-  height: number,
-  radius: number,
-) {
-  const r = Math.min(radius, width / 2, height / 2);
-  ctx.beginPath();
-  ctx.moveTo(x + r, y);
-  ctx.arcTo(x + width, y, x + width, y + height, r);
-  ctx.arcTo(x + width, y + height, x, y + height, r);
-  ctx.arcTo(x, y + height, x, y, r);
-  ctx.arcTo(x, y, x + width, y, r);
-  ctx.closePath();
-}
-
 function drawHeroHealth(
   ctx: CanvasRenderingContext2D,
   hp: number,
