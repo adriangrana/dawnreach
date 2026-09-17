73e975cf1e3632a9a86b3f13331ced3c3b5a62f6
Adrian Torres Graña <adriangrana@gmail.com>
2026-09-17 21:46:53 +0200

    load presence status styles

--- src/main.tsx
+++ src/main.tsx
@@ -36,6 +36,7 @@ import './platform-home-social-reference.css';
 import './platform-home-social-integration.css';
 import './platform-home-lower-strip-legibility.css';
 import './platform-home-chat.css';
+import './platform-presence-status.css';
 
 const BOOT_SPLASH_ID = 'dawnreach-boot-splash';
 const BOOT_SPLASH_MAX_WAIT_MS = 12_000;
