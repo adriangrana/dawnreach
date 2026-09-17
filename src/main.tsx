7a46b58ba0ded09e134847f522db45e5a9924a0b
Adrian Torres Graña <adriangrana@gmail.com>
2026-09-17 22:02:00 +0200

    load tabbed chat styles

--- src/main.tsx
+++ src/main.tsx
@@ -37,6 +37,7 @@ import './platform-home-social-integration.css';
 import './platform-home-lower-strip-legibility.css';
 import './platform-home-chat.css';
 import './platform-presence-status.css';
+import './platform-home-chat-tabs.css';
 
 const BOOT_SPLASH_ID = 'dawnreach-boot-splash';
 const BOOT_SPLASH_MAX_WAIT_MS = 12_000;
