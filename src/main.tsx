4ead5f55c3a9aca9eaeb96f5cf07c67f89f1ec77
Adrian Torres Graña <adriangrana@gmail.com>
2026-09-17 22:57:48 +0200

    load topbar right header polish

--- src/main.tsx
+++ src/main.tsx
@@ -39,6 +39,7 @@ import './platform-home-chat.css';
 import './platform-presence-status.css';
 import './platform-home-chat-tabs.css';
 import './platform-home-party-chat-polish.css';
+import './platform-home-topbar-right-polish.css';
 
 const BOOT_SPLASH_ID = 'dawnreach-boot-splash';
 const BOOT_SPLASH_MAX_WAIT_MS = 12_000;
