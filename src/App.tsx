93cb17fdf5f1dea03ddfe4e1013d8e4e551f509e
Adrian Torres Graña <adriangrana@gmail.com>
2026-09-18 22:47:55 +0200

    import structure network snapshot in app

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ import {
   createDawnreachGame,
   type DawnreachCreepNetworkSnapshot,
   type DawnreachRemoteHeroState,
+  type DawnreachStructureNetworkSnapshot,
 } from './game/createDawnreachGame';
 import {
   publishWorldEntityRuntime,
